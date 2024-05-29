669917879491e9022259f9c0f558d7724b72f7b0
ataklti21 <atuka2@gmail.com>
2024-05-29 03:42:27 -0400

    date and cal added

--- src/main/java/com/atuka/exercises/ExercisesMain.java
+++ src/main/java/com/atuka/exercises/ExercisesMain.java
@@ -3,7 +3,7 @@ package com.atuka.exercises;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+import java.lang.Error;
 public class ExercisesMain {
     public static void main(String[] args) {
         Exercises ex = new Exercises();
@@ -13,7 +13,7 @@ public class ExercisesMain {
         System.out.println("Date time -" + LocalDateTime.now());
         LocalDate date = LocalDate.of(2024, 9, 2);
         System.out.println(date);
-
+        System.out.println(isSquRoot(16));
     }
     public  static  boolean isSquRoot(int n){
         int sq = (int)Math.sqrt(n);
